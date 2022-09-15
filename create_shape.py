87d0afd79f06d1cdc1f2cea4e71b3068010e61d0
reza <mdshirazi@gmail.com>
2022-09-15 12:57:51 +0200

    added chirality functionality

--- create_shape.py
+++ create_shape.py
@@ -1,6 +1,8 @@
 #from ase import Atoms
 from ase.io import read, write
+from ase.build import make_supercell
 import numpy as np
+import pandas as pd
 from scipy.integrate import quad
 #from scipy.spatial.transform import Rotation
 #from scipy import special
@@ -9,10 +11,15 @@ from scipy.integrate import quad
 import argparse
 import sys
 import math
+import matplotlib.pyplot as plt
+from scipy.interpolate import splrep,splev
+# from scipy.signal import argrelextrema
+# from chiral.py import *
 
 parser = argparse.ArgumentParser(description="Create a structure of a 2D layer which follows a parametric function")
-parser.add_argument("--func", type=int, default=1, help="The parametric function will be (by default) 1 - sine, 2 - ellipsoidic, 3 - nanotube, 4 - hypotrochoid 5- two sines")
-parser.add_argument("--dir", type=int, help="Create the wrinkle along the specified lattice vector (default=the smaller of 1. or 2. vector)")
+parser.add_argument("--func", type=int, default=2, help="The parametric function will be (by default) \
+                    1 - sine, 2 - ellipsoidic, 3 - nanotube, 4 - hypotrochoid 5- two sines 6- numeric ")
+parser.add_argument("--dir", type=int, help="Create the wrinkle along the specified lattice vector (default=the smaller of 1 or 2. vector)")
 parser.add_argument("--A", type=float, required=False, default=0, help="Amplitude of the wave or the 1st radius")
 parser.add_argument("--length", type=float, required=False, default=0,help="Wavelength of the wave or the 2nd radius")
 parser.add_argument("--C", type=float, default=1.0, help="For some curves a 3rd parameter is needed.")
@@ -22,11 +29,35 @@ parser.add_argument("--adjust", type=int, default=1, help="Adjust the wavelength
 parser.add_argument("--out", type=str, help="Output file (default=L'length'A'A'.in)")
 parser.add_argument("--outtype", type=str, default='aims', help="Type of output file as used in ASE (default=aims)")
 parser.add_argument("--unit", type=int, default=0, required=False, help="for wrinkles, this option forces the number of unit cells")
+parser.add_argument("--chiral", type=int, nargs=2, default=0, required=False, help="chiral structure rolling vector, for the moment tested for hexagonal cell")
 
 
 args = parser.parse_args()
 
 #
+# m=5
+# n=1
+outfile='geometry_supercell.in'
+#
+
+def chiral_unitcell(inputfile,outfile,m,n):
+
+    ################# reading the geometry
+    single_cell = read(inputfile, format='aims')
+    gcd_value = math.gcd(m,n)
+    n=n/gcd_value
+    m=m/gcd_value
+    ##formula for hexagonal unit cell
+    l1=-(2*n+m)
+    l2=2*m+n
+    P=[[m,n,0],[l1,l2,0],[0,0,1]]
+    final_cell = make_supercell(single_cell,P,wrap=False)  
+    final_cell.rotate(np.degrees(np.arctan(-final_cell.get_cell()[0][1]/final_cell.get_cell()[0][0])),(0,0,1),rotate_cell=True)
+    positions = final_cell.get_positions()
+    positions[:,2] = 0 # make things on the ground
+    final_cell.set_positions(positions)
+    write(outfile,final_cell, format='aims',wrap=False)
+    return final_cell,l1,l2
 
     
 if (args.func == 1):
@@ -96,13 +127,46 @@ elif (args.func == 5):
     a4=2.91407
     a5=-6.23166
     return quad(lambda t: np.sqrt(math.pow(a0*a1*np.cos(a1*t+a2)+a3*a4*np.cos(a4*t+a5),2)+math.pow(b/2/np.pi,2)), x1, x2)[0]
+elif (args.func == 6):
+    # file = open('wrinkle_profile.txt') 
+    filename='wrinkle_profile.txt'
+    data = pd.read_csv(filename, sep="\s+", header=None)
+    # lines = file.readlines()
+    positions=data.iloc[3:124,2:4] #selecting the wrinkle+MoS2 
+    positions = positions.replace({',':'.'},regex=True).apply(pd.to_numeric,1)
+    positions_A =positions*10**10 #change of unit to Angstrom
+    x=np.array(positions_A.iloc[::,0])
+    y=np.array(positions_A.iloc[::,1])
+    # x_m= list(map( lambda t: ((2*np.pi)/(max(positions_A.iloc[::,0])-min(positions_A.iloc[::,0])))*t, positions_A.iloc[::,0]))
+    # y_m= list(map( lambda t: (2/(max(positions_A.iloc[::,1])-min(positions_A.iloc[::,1])))*t, positions_A.iloc[::,1]))
+    Bspline=  splrep(positions_A.iloc[::,0],positions_A.iloc[::,1] ,k=4)
+    New_K=400
+    t =np.linspace(0,9500,New_K)
+    plt.plot(t,splev(t,Bspline),'o')
+    # plt.xlim([0,8200])
+    dy=[]
+    for i in range(len(t)-1): dy.append((splev(t,Bspline)[i+1]-splev(t,Bspline)[i])/(t[i+1]-t[i]))
+    ds=[]
+    for i in range(len(dy)): ds.append(np.sqrt(1+dy[i]**2)*(t[2]-t[1]))
+
+    arclength_curve= sum(ds)
+    #
+    curvature=[]
+    dyy=[]
+    for i in range(len(t)-2):
+      # dyy.append((dy[i+1]-dy[i])/((t[i+1]-t[i])))
+        dyy.append((splev(t,Bspline)[i+2]-2*splev(t,Bspline)[i+1]+splev(t,Bspline)[i])/((t[i+1]-t[i])**2))
+
+    for i in range(len(t)-2):
+        curvature.append(dyy[i]/((1+dy[i]**2)**(3/2)))
 
 else:
   print(" [fatal error] parametric function ",args.func, " not defined")
   sys.exit()
 
 wrinkle = args.dir
-
+chiral = args.chiral
+m,n =args.chiral
 ###message section, for making the code neat I put all the messages about the inputs here
 if wrinkle > 1 or wrinkle <0:
     print( "[WARNING] wrinkle direction ", args.dir, " not defined") 
@@ -116,15 +180,18 @@ if unit != 0 and args.func == 3:
     sys.exit()   
 if (args.A == 0 or args.length == 0):
         print('[WARNING] no amplitude or wavelength is not given if it is a unitcell only case there will be no problem')
-if (args.A == 0 or args.length == 0) and (args.func != 2):
+if (args.A == 0 or args.length == 0) and (args.func != 2 and args.func !=6 ):
         sys.exit('[fatal error ] for the selected shapes I need wavelength and amplitude')
-if (args.A == 0 and args.length == 0 and args.func == 2 and args.unit == 0 ):   
-        sys.exit('[fatal error] for making a cycloidic structure without amplitude and wavelength I need the number of unit cells')
+if (args.A == 0 and args.length == 0 and args.func == 2 and args.unit == 0 and args.chiral ==0):   
+        sys.exit('[fatal error] for making a cycloidic structure without amplitude and wavelength I need the number of unit cells if the structure is chiral provide rolling vector')
 ####################################
 inputfile = args.struct
 
-single_cell=read(inputfile, format=args.intype)
-single_cell.wrap(pbc=[1,1,0])
+if chiral !=0:
+    final_cell,l1,l2 =chiral_unitcell(inputfile, outfile, m, n)
+    single_cell = final_cell
+else:
+     single_cell=read(inputfile, format=args.intype)
 #single_cell.wrap(pbc=[1,1,1],center=(0.0,0.0,0.0))
 print(single_cell.get_positions())
 
@@ -152,9 +219,15 @@ else:
 alat=single_cell.get_cell_lengths_and_angles()[wrinkle]
 
 #Amplitude and wavelength of the wrinkle in Angstrom
-if (args.A ==0 and args.length == 0):
+if (args.A ==0 and args.length == 0 and chiral ==0):
     amplitude=alat*args.unit/(2*np.pi)
     wavelength=4*amplitude
+elif (chiral !=0 and args.A==0):
+    wavelength = np.linalg.norm(vec1)
+    amplitude =0.25* wavelength
+elif (chiral !=0 and args.A !=0):
+    wavelength = np.linalg.norm(vec1)
+    amplitude = args.A
 else:
    amplitude=args.A
    wavelength=args.length
@@ -174,7 +247,10 @@ else:
   multipi = 1
 
 #circumference=wavelength*special.ellipe(np.sqrt(1-math.pow(amplitude/(wavelength/4),2)))
-circumference=arclength(0,2*np.pi*multipi,amplitude,wavelength,cparam)
+if (args.func == 6):
+    circumference= arclength_curve
+else:
+    circumference=arclength(0,2*np.pi*multipi,amplitude,wavelength,cparam)
 
 print("[info] Initial wavelength ", wavelength)
 print("[info] Initial amplitude ", amplitude)
@@ -187,6 +263,14 @@ print("[info] How often does the cell fits into this (rounded) ",howoften)
 
 length=howoften*alat
 print("[info] The resulting length of the path would be ",length)
+
+if (args.func == 6):
+    if (length ==arclength_curve):
+        print("final length is determined")
+    else:
+        # x_m= list(map( lambda t: ((2*np.pi)/(max(positions_A.iloc[::,0])-min(positions_A.iloc[::,0])))*t, positions_A.iloc[::,0]))
+        # y_m= list(map( lambda t: (2/(max(positions_A.iloc[::,1])-min(positions_A.iloc[::,1])))*t, positions_A.iloc[::,1]))
+        length
 
 if abs(length-circumference)==0:
   print("[info] initial is final") 
@@ -202,7 +286,7 @@ else:
  # print('amptest after amptest', amptest)
   ctest=cparam
   circumtest=circumference
-  if unit == 0:
+  if (unit == 0 and args.func !=6):
       while factor*(circumtest-length) < 0:
          
         if (args.func == 3):
@@ -223,6 +307,8 @@ else:
         if wavetest < 0 or amptest < 0 or ctest < 0:
           print("wavetest, amptest ",wavetest,amptest)
           sys.exit('Wavelength or amplitude negative! Reasonable wave form cannot be found.')
+  if (args.func ==6):
+      a=1
   else: 
   #    print('amptest in the else ',amptest)
        while factor*(circumtest-length) < 0:
@@ -278,7 +364,7 @@ else:
 rotation_axis=rotation_axis/np.linalg.norm(rotation_axis)
 print('[info] rotation axis:',rotation_axis)
 
-stepping = 0.00001 * np.pi
+stepping = 0.001 * np.pi
 t0 = 0
 i = 0
 counting=1
